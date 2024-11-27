d1caa5dfe1058ab8c8645937879c93e0bcc9fb42
paulmwatson <github.com@paulmwatson.com>
2024-11-27 23:03:19 +0200

    Tweak home page

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,18 +1,19 @@
 import {
-    Card,
-    CardDescription,
-    CardHeader,
-    CardTitle,
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card"
 
 export default function Page() {
-    return (
-        <Card>
-            <CardHeader>
-                <CardTitle>Skylink</CardTitle>
-                <CardDescription>An inverted social network viewer for Bluesky: putting links first.</CardDescription>
-            </CardHeader>
-        </Card>
-
-    )
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>Skylink</CardTitle>
+          <CardDescription>An inverted social network viewer for Bluesky: putting links first.</CardDescription>
+        </CardHeader>
+      </Card>
+    </div>
+  )
 }
